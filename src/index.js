88d36414c5af84da3287b296d83a1d3579c1e90d
Tiago Bai <tiagoxbai@gmail.com>
2016-10-19 16:35:46 -0200

    Organizing the js source

--- src/index.js
+++ src/index.js
@@ -1,74 +1,21 @@
 
-class ProgressBar {
-
-  constructor (id, initialValue = 0, limit = 100){
-    this.id = id;
-    this.value = initialValue;
-    this.limit = limit;
-  }
-
-  buildElement(){
-    this.el = document.createElement("div");
-    this.el.id = this.id;
-    this.el.classList.add("progress-bar");
-    this.el.innerHTML =`
-      <div class="bar"></div>
-      <div class="level"></div>
-      <div class="text">${this.value} %</div>
-    `;
-
-    this.levelEl = this.el.querySelector(".level");
-    this.textEl = this.el.querySelector(".text");
-
-    this.onChange();
-  }
-
-  convertValue(num){
-    return parseInt (100*num/this.limit, 10);
-  }
-
-  onChange(){
-    this.textEl.innerText = `${this.convertValue(this.value)} %`;
-    this.levelEl.style.width = `${this.convertValue(this.value)}%`;
-  }
+import ProgressBar from 'progress-bar';
 
 
-  setValue(num){
-    // validate the number type and range
-    if (typeof num === "number" && num >= 0 && num <=this.limit){
-      this.value = num;
-      this.onChange();
-    }
-  }
-
-  incValueBy(num){
-    let newValue = this.value + num;
-    if (newValue > this.limit){
-      newValue = this.limit
-    } else if (newValue <0){
-      newValue = 0;
-    }
-    this.setValue(newValue);
-  }
-
-  render (containerEl){
-
-    if (!containerEl)
-      throw new Error("Container element not found: " + containerSelector);
-
-    this.buildElement();
-    containerEl.appendChild(this.el);
-  }
 
 
-}
 
 
-// Build the HTML
+// ********************************************************************************
+// Build the HTML and setup the components
+// ********************************************************************************
 
 let data = null;
 let progressBars = [];
 
+
+
+// fetch the data from the api and setup the components
 fetch('https://pb-api.herokuapp.com/bars')
 .then(function(response) {
   return response.json().then( json => {
@@ -79,8 +26,11 @@ fetch('https://pb-api.herokuapp.com/bars')
 });
 
 
+
+
+// Render progress bars
 function setupBars(){
-  // Render progress bars
+
   const barsContainerEl = document.querySelector(".progress-bars-container");
 
   data.bars.forEach((initialVal, i) => {
@@ -91,7 +41,10 @@ function setupBars(){
 
 }
 
+
+
 function setupActionButtons(){
+
   // Render action buttons
   const actionContainerEl = document.querySelector(".actions");
 
@@ -105,6 +58,7 @@ function setupActionButtons(){
     selectEl.appendChild(optionEl);
   });
 
+  // create the action buttons
   data.buttons.forEach(num => {
     let btn = document.createElement('button');
     btn.setAttribute("value", num);
@@ -112,6 +66,7 @@ function setupActionButtons(){
     actionContainerEl.appendChild(btn);
   });
 
+  // setup the event listeners
   actionContainerEl.addEventListener("click", event => {
     let target = event.target;
     if (event.target instanceof HTMLButtonElement){
